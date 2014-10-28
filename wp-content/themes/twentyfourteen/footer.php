e38e8602f694b5bd43450844d6cc50423e4227da
wen.li <wen.li@airborne-es.com>
2014-10-28 14:00:57 +0800

    add all myblog file and folder to github

---
+++ wp-content/themes/twentyfourteen/footer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+<head>
+	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+</head>
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<?php get_sidebar( 'footer' ); ?>
+
+			<!--<div class="site-info">
+				<?php do_action( 'twentyfourteen_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
+			</div>--><!-- .site-info -->
+			
+			<div class="site-info">
+				Copyright &copy 2014 lanruyi.net.<br />
+				<a href="www.lanruyi.net">web by lanruyi.net</a>|<a href="http://www.miitbeian.gov.cn/">À’ICP±∏14038561∫≈-1</a>
+			</div>
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
